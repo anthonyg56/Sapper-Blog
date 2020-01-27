6c8c2698c2700061cf61da0caf34c3c5c63c00a3
anthonyg56 <anthonyg5256@gmail.com>
2020-01-27 09:37:05 -0500

    Added Netlify CMS

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,23 @@
 <script>
 	import Home from '../components/Home/Home.svelte'
+	import { onMount } from 'svelte'
+
+  	onMount(() => {
+		if (window.netlifyIdentity) {
+			window.netlifyIdentity.on("init", user => {
+				if (!user) {
+					window.netlifyIdentity.on("login", () => {
+						document.location.href = "/admin/"
+					})
+				}
+			})
+		}
+  	})
 </script>
 
 <svelte:head>
 	<title>A Creatives Blog</title>
+	<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
 </svelte:head>
 
 <Home />
